6ca6ede57d97b19f28f04a53e688f60b2ab3db33
Juan Ayala <juan_20_as@hotmail.com>
2018-10-19 18:59:01 -0500

    login
    lleva un nuevo css para lo del login y registro y unos comentarios en el js del reproductor

--- reproductor.js
+++ reproductor.js
@@ -10,11 +10,11 @@
           height: '250',
           width: '270',
           playerVars: {'color':'white','cc_load_policy':0,'disablekb':1,'fs':0,
-           'iv_load_policy':3,'modestbranding':1, 'rel':0,
+           'iv_load_policy':3,'modestbranding':1, 'rel':0,        //opciones del reproductor, color, informacion 
             'showinfo':0, 
             'listType':'playlist',
-            'list':'PLWO2m0Y4HA5czJ_lPT0ZIqKAC_01LaNFC',
-             'loop':1
+            'list':'PLWO2m0Y4HA5czJ_lPT0ZIqKAC_01LaNFC',  //playlist a reproducir
+             'loop':1                                     //la playlist estara en un loop
             },
           events: {
             'onReady': onPlayerReady,
@@ -23,9 +23,9 @@
       }
 
       function onPlayerReady(event) {
-        event.target.setVolume(20);
+        event.target.setVolume(20);     //volumen de los videos
         player.setShuffle(true);
-        event.target.playVideo();
+        event.target.playVideo();       //reproduce los videos
 
       }
 
@@ -34,5 +34,5 @@
       }
 
       function stopVideo() {
-        player.stopVideo();
+        player.stopVideo();   
       }
