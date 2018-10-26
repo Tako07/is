e788daf54bcac04b3c05dad4e3a41edbfd02e0c1
Takeshi Nakamura <g.t.n.ramirez@gmail.com>
2018-10-25 22:50:57 -0500

    verifica en el html y javascript primera  negocio

--- registroN.php
+++ registroN.php
@@ -23,25 +23,25 @@
 					<img id="icono" src="iconos/ic_profile_v3.png">
 				</figure>
 				<form action="registroNegocio.php" name="formulario" id="formulario" method="get" onsubmit='return validaFormulario();'>
-					<input id='NombNegocio' class="formato" type="text" name="negocio" placeholder="Nombre negocio">
+					<input id='NombNegocio' class="formato" type="text" name="negocio" placeholder="Nombre negocio" required>
           <div id="nomApe">
-  					<input id='nombre' class="formato" type="text" name="nombre" placeholder="Nombre">
-  					<input id='apell' class="formato" type="text" name="apellido" placeholder="Apellido">
+  					<input id='nombre' class="formato" type="text" name="nombre" placeholder="Nombre" required>
+  					<input id='apell' class="formato" type="text" name="apellido" placeholder="Apellido" required>
           </div>
-          <input id='correo' class="formato" type="email" name="correo" placeholder="Correo electrónico" >
+          <input id='correo' class="formato" type="email" name="correo" placeholder="Correo electrónico" required>
 					<div class="posicion">
-						<input id='contrasena' class="formatopass" type="password" name="contrasena" value="" placeholder="Contraseña">
+						<input id='contrasena' class="formatopass" type="password" name="contrasena" value="" placeholder="Contraseña" required>
 						<figure class="ojo">
 							<img id="ojo" src="iconos/ojo.png" onclick="mostrarpass(0);">
 						</figure>
 					</div>
 					<div class="posicion">
-						<input id='contrasena1' class="formatopass" type="password" name="contraseña1" value="" placeholder="Contraseña">
+						<input id='contrasena1' class="formatopass" type="password" name="contraseña1" value="" placeholder="Contraseña" required>
 						<figure class="ojo">
-							<img id="ojo" src="iconos/ojo.png" onclick="mostrarpass(1);">
+							<img id="ojo" src="iconos/ojo.png" onclick="mostrarpass(1);" required>
 						</figure>
 					</div>
-					<input type="checkbox" name="terminos" id='terminos' value="Acepto"> Aceptas nuestros <a href="https://www.trivago.es/terminos-y-condiciones">terminos y condiciones.</a>
+					<input type="checkbox" name="terminos" id='terminos' value="Acepto" required> Aceptas nuestros <a href="https://www.trivago.es/terminos-y-condiciones">terminos y condiciones.</a>
 					<div class="botones1">
 						<input type="submit"  class="formButton1" value="Registrarse" >
 						<input type="reset"  name="Cancelar" class="formButton1" value="Quiero ser un cliente">
