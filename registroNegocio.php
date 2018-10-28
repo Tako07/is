0eabe70c67e3f1ad410edf6cb24331e98bb37aa7
Takeshi Nakamura <g.t.n.ramirez@gmail.com>
2018-10-28 14:10:20 -0600

    Comentarios en código

--- registroNegocio.php
+++ registroNegocio.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <?php
+	//Obtención de variables del formulario
 	$negocio=$_POST['negocio'];
 	$nombre=$_POST['nombre'];
 	$apellido=$_POST['apellido'];
 	$correo=$_POST['correo'];
 	$pass=$_POST['contrasena'];
 	$idUsr;
+	//Conección con la base de datos
 	$con=mysqli_connect("localhost" , "root" , "Privada" , "data_service_in") or die("No se pudo conectar: ".mysql_error());
 	if(mysqli_connect_errno()){
 		printf("Falló la conexión: %s\n",mysqli_connect_errno());
 	}
+	//Consulta para verificar si el correo existe en BD
 	$q="SELECT * FROM usuario WHERE correo='".$correo."';";
+	//ejecución de la consulta
 	$result=mysqli_query ($con,$q);
+	//Si existe el correo en la BD
 	if(mysqli_num_rows($result)>0){
 		echo '<script>alert("Correo ya registrado, pruebe con otro");
 		history.back();</script>';
-	}else {echo 'acá';
+	//si no existe el correo en la BD insertar el usuario
+	}else {
+		//Inserción del usuario en la bd
 		$sql="INSERT INTO usuario (nombre,apellido,correo,password) VALUES ('".$nombre."','".$apellido."','".$correo."',MD5('".$pass."'));";
 		if(mysqli_query($con,$sql)){
-				$idUsr=mysqli_insert_id($con);
+			//Obtención del id del usuario recien insertado
+			$idUsr=mysqli_insert_id($con);
 		}
-
-
-
 	}
 
  ?>
@@ -37,16 +42,17 @@
 	</head>
 
 	<body>
-		<div id="app">
-			<header id="cabecera">
-					<figure class="home" onclick="home();">
-						<img id="home" src="iconos/ic_home_v3.png">
-					</figure>
-					<h1 id="texto">Services In</h1>
-					<button id="botonesN" onclick="login();">Iniciar Sesion</button>
+		<div id="app"><!--caja principal del documento html-->
+			<header id="cabecera"><!--Caja de barra superior de la página-->
+				<figure class="home" onclick="home();">
+					<img id="home" src="iconos/ic_home_v3.png">
+				</figure>
+				<h1 id="texto">Services In</h1>
+				<button id="botonesN" onclick="login();">Iniciar Sesion</button>
 			</header>
-			<section id="seccionc2RN">
+			<section id="seccionc2RN"><!--Sección principal de la segunda´página del registro del negocio-->
 				<h1>Llena el formulario <br> para continuar</h1>
+				<!--Formulario de datos -->
 				<form action="FinRegistro.php" name="form" id="formulario" method="POST">
 					<input class="formatoIn2R" type="text" name='negocio' placeholder="Piñas coladas Martita" value='<?php echo $negocio; ?>' readonly>
 					<p class="formatoL2R">Calle:</p>
@@ -66,12 +72,12 @@
 					<p class="formatoL2R">Categoría del negocio:</p>
 					<div class="lista">Categorias |
 						<select id="desplegable" name='categoria'>
-   						<option value='1'>Categoria 1</option>
-   						<option value='2'>Categoria 2</option>
-   						<option value='3'>Categoria 3</option>
-   						<option value='4'>Categoria 4</option>
-   						<option value='5'>Categoria 5</option>
-   						<option value='6'>Categoria 6</option>
+   							<option value='1'>Categoria 1</option>
+   							<option value='2'>Categoria 2</option>
+   							<option value='3'>Categoria 3</option>
+   							<option value='4'>Categoria 4</option>
+   							<option value='5'>Categoria 5</option>
+   							<option value='6'>Categoria 6</option>
 						</select>
 						<input id='idUsuario' type='text' name='idUsr' value='<?php echo $idUsr; ?>' style='display:none'>
 					</div>
@@ -79,7 +85,6 @@
 						<input id="registroFin" type="submit"  class="formButton1" value="Registrarse" >
 					</center>
 				</form>
-<!--sujefa-->
 			</section>
 		</div>
 	</body>
